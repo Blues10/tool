80a627219bdec98674c361e631023e89a41e4d3c
blues <384953206@qq.com>
2019-02-14 16:56:02 +0800

    qihang

---
+++ setsail/src/main/java/com/setsail/dao/SysLogMapper.java
@@ -0,0 +1,20 @@
+package com.setsail.dao;
+
+import com.setsail.model.SysLog;
+import com.setsail.model.SysLogWithBLOBs;
+
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysLogWithBLOBs record);
+
+    int insertSelective(SysLogWithBLOBs record);
+
+    SysLogWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysLogWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(SysLogWithBLOBs record);
+
+    int updateByPrimaryKey(SysLog record);
+}
---
+++ setsail/src/main/java/com/setsail/dao/SysRoleAclMapper.java
@@ -0,0 +1,17 @@
+package com.setsail.dao;
+
+import com.setsail.model.SysRoleAcl;
+
+public interface SysRoleAclMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleAcl record);
+
+    int insertSelective(SysRoleAcl record);
+
+    SysRoleAcl selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleAcl record);
+
+    int updateByPrimaryKey(SysRoleAcl record);
+}

5f3b79ae143ae0407f9677b7fd3ca5be8504fe6c
blues <384953206@qq.com>
2019-03-15 16:46:06 +0800

    qihang

--- setsail/src/main/java/com/setsail/controller/SysUserController.java
+++ setsail/src/main/java/com/setsail/controller/SysUserController.java
@@ -1,15 +1,19 @@
 package com.setsail.controller;
 
 
+import com.setsail.beans.PageQuery;
+import com.setsail.beans.PageResult;
 import com.setsail.common.JsonData;
-import com.setsail.param.DeptParam;
 import com.setsail.param.UserParam;
 import com.setsail.service.SysUserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
+@Controller
 @RequestMapping("/sys/user")
 public class SysUserController {
 
@@ -40,5 +44,10 @@ public class SysUserController {
         return JsonData.success();
     }
 
-
+    @RequestMapping("/page.json")
+    @ResponseBody
+    public JsonData page(@RequestParam("deptId") int deptId , PageQuery pageQuery) {
+        PageResult result = sysUserService.getPageByDeptId(deptId, pageQuery);
+        return JsonData.success(result);
+    }
 }
--- setsail/src/main/java/com/setsail/service/SysUserService.java
+++ setsail/src/main/java/com/setsail/service/SysUserService.java
@@ -1,16 +1,20 @@
 package com.setsail.service;
 
 import com.google.common.base.Preconditions;
+import com.setsail.beans.PageQuery;
+import com.setsail.beans.PageResult;
 import com.setsail.dao.SysUserMapper;
 import com.setsail.exception.ParamException;
 import com.setsail.model.SysUser;
 import com.setsail.param.UserParam;
 import com.setsail.util.BeanValidator;
+import com.setsail.util.MD5Util;
 import com.setsail.util.PasswordUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class SysUserService {
@@ -28,10 +32,9 @@ public class SysUserService {
         }
         String password = PasswordUtil.randomPassword();
         //TODO:
-        password = "123456";
-        String encryptedPassword = "";
+        String encryptedPassword = MD5Util.encrypt(password);
         SysUser user = SysUser.builder().username(param.getUsername()).telephone(param
-                .getTelephone()).mail(param.getMail()).password(password).deptId(param.getDeptId())
+                .getTelephone()).mail(param.getMail()).password(encryptedPassword).deptId(param.getDeptId())
                 .status(param.getStatus()).remark(param.getRemark()).build();
         user.setOperator("system"); //TODO
         user.setOperateIp("127.0.0.1"); //TODO
@@ -51,7 +54,7 @@ public class SysUserService {
             throw new ParamException("邮箱已被占用");
         }
         SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
-        Preconditions.checkNotNull(param, "待更新的用户不存在");
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
         SysUser after = SysUser.builder().id(param.getId()).username(param.getUsername()).telephone(param
                 .getTelephone()).mail(param.getMail()).deptId(param.getDeptId()).status(param.getStatus())
                 .remark(param.getRemark()).build();
@@ -59,10 +62,24 @@ public class SysUserService {
     }
 
     public boolean checkEmailExist(String mail, Integer userId) {
-        return false;
+        return sysUserMapper.countByMail(mail, userId) > 0;
     }
 
-    public boolean checkTelephoneExist(String mail, Integer userId) {
-        return false;
+    public boolean checkTelephoneExist(String telephone, Integer userId) {
+        return sysUserMapper.countByTelephone(telephone, userId) > 0;
+    }
+
+    public SysUser findByKeyword(String keyword) {
+        return sysUserMapper.findByKeyword(keyword);
+    }
+
+    public PageResult<SysUser> getPageByDeptId(int deptId, PageQuery page) {
+        BeanValidator.check(page);
+        int count = sysUserMapper.countByDeptId(deptId);
+        if (count > 0) {
+            List<SysUser> list = sysUserMapper.getPageByDeptId(deptId, page);
+            return PageResult.<SysUser>builder().total(count).data(list).build();
+        }
+        return PageResult.<SysUser>builder().build();
     }
 }

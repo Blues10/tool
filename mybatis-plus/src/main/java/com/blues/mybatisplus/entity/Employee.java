f22816e398fd704abe7e6dd6f4610c128f084222
blues <384953206@qq.com>
2019-07-27 23:58:24 +0800

    mybatis-plus

---
+++ mybatis-plus/src/main/java/com/blues/mybatisplus/entity/Employee.java
@@ -0,0 +1,20 @@
+package com.blues.mybatisplus.entity;
+
+import lombok.Data;
+
+/**
+ * Created by blues on 2019/7/27 23:49
+ */
+@Data
+public class Employee {
+
+    private Integer id ;
+
+    private String lastName;
+
+    private String email ;
+
+    private Integer gender ;
+
+    private Integer age ;
+}
---
+++ mybatis-plus/src/main/java/com/blues/mybatisplus/mapper/EmployeeMapper.java
@@ -0,0 +1,10 @@
+package com.blues.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.blues.mybatisplus.entity.Employee;
+
+/**
+ * Created by blues on 2019/7/27 23:52
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+}
